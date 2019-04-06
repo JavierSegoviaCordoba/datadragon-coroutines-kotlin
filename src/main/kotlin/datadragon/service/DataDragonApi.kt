c554f2c28d5d125cc438e1084cb165c7f37ead33
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2019-04-06 21:51:51 +0200

    Change file names

--- src/main/kotlin/datadragon/service/DataDragonApi.kt
+++ src/main/kotlin/datadragon/service/DataDragonApi.kt
@@ -6,23 +6,18 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
+object DataDragonApi {
+    private const val baseUrl = "https://ddragon.leagueoflegends.com/"
 
-class DataDragonApi {
-
-    companion object {
-        private const val baseUrl = "https://ddragon.leagueoflegends.com/"
-
-        private val objectMapper = jacksonObjectMapper().apply {
-            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // true for testing
-            configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-        }
-
-        private val retrofit = Retrofit.Builder().apply {
-            baseUrl(baseUrl)
-            addConverterFactory(JacksonConverterFactory.create(objectMapper))
-        }.build()
-
-        val service: DataDragonService by lazy { retrofit.create(DataDragonService::class.java) }
+    private val objectMapper = jacksonObjectMapper().apply {
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // true for testing
+        configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
     }
 
+    private val retrofit = Retrofit.Builder().apply {
+        baseUrl(baseUrl)
+        addConverterFactory(JacksonConverterFactory.create(objectMapper))
+    }.build()
+
+    val service: DataDragonService by lazy { retrofit.create(DataDragonService::class.java) }
 }
